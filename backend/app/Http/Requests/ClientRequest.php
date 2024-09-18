b45b99cfdccb65b5f6c11086eb7fe06d0c9b29f3
Koemsran <koemsran168@gamil.com>
2024-09-18 14:39:29 +0700

    clean user

---
+++ backend/app/Http/Requests/ClientRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ClientRequest extends DefaultRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required','string','max:255'],
+            'age' => ['required','max:255'],
+            'phone' => ['required','string','max:15','unique:clients,phone_number,'],
+        ];
+    }
+}
